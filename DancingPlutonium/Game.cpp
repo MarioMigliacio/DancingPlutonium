0634135a51b21421de22abb75f06d943d11c8d24
lordcheezus <migliacio@hotmail.com>
2017-04-21 16:05:54 -0700

    Game.h and Game.cpp are started. Now comes to development of each module pertinant to the game.

---
+++ DancingPlutonium/Game.cpp
@@ -0,0 +1,95 @@
+#include "Game.h"
+
+// Static variable declarations:
+sf::Uint32 Game::m_state = s_uninitialized;
+sf::RenderWindow Game::m_window;
+
+void Game::Initialize()
+{
+	// Ensure that the initialization takes place correctly here and for the first time.
+	if (m_state != current_state::s_uninitialized)
+	{
+		return;
+	}
+
+	// Create the Rendering Window.
+	sf::VideoMode mode = sf::VideoMode::getDesktopMode();
+	m_window.create(sf::VideoMode(mode.width, mode.height, mode.bitsPerPixel), "Dancing Plutonium", sf::Style::Fullscreen);
+
+	m_state = current_state::s_menu;
+
+	while (!IsQuitter())
+	{
+		Run();
+	}
+
+	m_window.close();
+}
+
+void Game::Run()
+{
+	switch (m_state)
+	{
+		case current_state::s_menu:
+			Menu();
+			break;
+		case current_state::s_playing:
+			Play();
+			break;
+		case current_state::s_options:
+			Options();
+			break;
+		case current_state::s_scoreboard:
+			ScoreScreen();
+			break;
+		case current_state::s_levelwin:
+			WinLevel();
+			break;
+		case current_state::s_gameover:
+			GameOver();
+			break;
+		default:
+			break;
+	}
+}
+
+void Game::Menu()
+{
+	m_window.setTitle("Main Menu");
+	// get around to creating the menu class.
+}
+
+void Game::Play()
+{
+	m_window.setTitle("Playing");
+	// get around to creating the playing class.
+}
+
+void Game::WinLevel()
+{
+	m_window.setTitle("Mission Success");
+	// get around to creating the mission class.
+}
+
+void Game::GameOver()
+{
+	m_window.setTitle("Game Over");
+	// get around to creating the gameover class.
+}
+
+void Game::Options()
+{
+	m_window.setTitle("Options");
+	// get around to creating the options class.
+}
+
+void Game::ScoreScreen()
+{
+	m_window.setTitle("Scoreboard");
+	// get around to creating the scoreboard class.
+}
+
+bool Game::IsQuitter()
+{
+	return m_state == current_state::s_quit;
+}
--- DancingPlutonium/Game.h
+++ DancingPlutonium/Game.h
@@ -3,6 +3,39 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <random>
-#include "EnumTypes.h"
-#include "Button.h"
-#include "StarField.h"
+
+class Game
+{
+public:
+	Game() {}
+	~Game() {}
+
+	static void Initialize();
+
+private:
+	static void Run();
+	static void Menu();
+	static void Play();
+	static void WinLevel();
+	static void GameOver();
+	static void Options();
+	static void ScoreScreen();
+	static bool IsQuitter();
+
+private:
+	enum current_state
+	{
+		s_uninitialized,
+		s_menu,
+		s_playing,
+		s_options,
+		s_scoreboard,
+		s_levelwin,
+		s_gameover,
+		s_quit
+	};
+
+private:
+	static sf::RenderWindow m_window;
+	static sf::Uint32 m_state;
+};
