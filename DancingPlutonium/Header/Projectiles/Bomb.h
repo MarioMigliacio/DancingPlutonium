71d3fa9ae1207a6701b4f4a10ff7ed142da5d991
MarioMigliacio <migliacio@hotmail.com>
2018-08-25 22:54:27 -0700

    Added some bomb stuff for the foundation of the bomb object. It worked in the playground so just create bomb to mimic a basic projectile and then explode and turn into the animation sprite and scale it up and clean up afterwards

---
+++ DancingPlutonium/Header/Projectiles/Bomb.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include<SFML/Graphics.hpp>
+#include "AnimatedSprite.h"
+
+namespace DancingPlutonium
+{
+	/* Bomb class is the bomb diggity. It acts like a projectile but does immense damage. */
+	class Bomb
+	{
+	public:
+		#pragma region Ctor/Dtors
+
+		/* Bomb constructor accepting RenderTarget, and Position input parameters */
+		Bomb(const sf::Vector2f& _pos);
+		/* Bomb copy constructor */
+		Bomb(const Bomb& _ref) {}
+		/* BasicBullet destructor */
+		~Bomb() {}
+
+		#pragma endregion
+
+		#pragma region Methods
+
+		/* Sets this projectiles sprite, texture and default position */
+		void SetSprite(const sf::Vector2f& _origin);
+		/* Update this projectile in the world based on the clock */
+		void Update(float _dt);
+		// Draw this projectile in the world
+		void Draw(sf::RenderTarget& _rt);
+		/* Returns true if this projectile is active, false otherwise */
+		bool GetActiveState(const sf::RenderTarget& _rt);
+		/* Returns the sprites representing the shots for this projectile */
+		sf::Sprite GetSprite();
+
+		#pragma endregion
+
+	private:
+		#pragma region Default values
+
+		Animation* currentAnimation;
+		sf::Texture texture;
+		AnimatedSprite animatedSprite;
+
+		const float defaultDamage = 200.f;			/* Represents a defaultDamage that is safe to use for this projectile */
+		const float defaultSpeed = 200.f;			/* Represents a defaultSpeed that is safe to use for this projectile */
+
+		#pragma endregion
+	};
+}
---
+++ DancingPlutonium/Source/Projectiles/Bomb.cpp
@@ -0,0 +1,5 @@
+#include "Bomb.h"
+
+DancingPlutonium::Bomb::Bomb(const sf::Vector2f & _pos)
+{
+}
